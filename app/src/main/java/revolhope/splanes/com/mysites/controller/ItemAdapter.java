8eecc5ad183529b40d97c4cb3fa5d75b9bc513a9
splanes <155541.clot@fje.edu>
2018-06-06 12:41:37 +0200

    item -> working on it

--- app/src/main/java/revolhope/splanes/com/mysites/controller/ItemAdapter.java
+++ app/src/main/java/revolhope/splanes/com/mysites/controller/ItemAdapter.java
@@ -6,35 +6,30 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Objects;
 
 import revolhope.splanes.com.mysites.R;
-import revolhope.splanes.com.mysites.helper.FlipAnimator;
 import revolhope.splanes.com.mysites.model.Item;
-import revolhope.splanes.com.mysites.model.Resource;
 
 public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.Holder>
 {
-
-    private SparseBooleanArray sparseArrayIsSwapped;
 
     private Context context;
     private LayoutInflater inflater;
     private List<Item> items;
     private List<Holder> holders;
     private FragmentManager fragmentManager;
+    private ViewGroup viewGroup;
 
     public ItemAdapter(@NonNull Context context, FragmentManager fragmentManager)
     {
@@ -42,14 +37,18 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.Holder>
         this.inflater = LayoutInflater.from(context);
         this.fragmentManager = fragmentManager;
         holders = new ArrayList<>();
-        sparseArrayIsSwapped = new SparseBooleanArray();
     }
 
     @NonNull
     @Override
     public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
     {
-        Holder holder = new Holder(inflater.inflate(R.layout.holder_item, parent, false), context, fragmentManager);
+        if(viewGroup == null)
+        {
+            viewGroup = parent;
+        }
+
+        Holder holder = new Holder(inflater.inflate(R.layout.holder_item, parent, false), fragmentManager);
         holders.add(holder);
         return holder;
     }
@@ -61,38 +60,58 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.Holder>
 
         if (items != null && items.size() > position)
         {
-         
-            if (holder.isSwapped)
-            {
-            
-            }
-            else
-            {
-            }
-         
+
             Item item = items.get(position);
-            holder.textView_name.setText(item.getName());
+
+            holder.frontFragment.context = context;
+            holder.backFragment.context = context;
+            holder.frontFragment.findViews(inflater, viewGroup);
+            holder.backFragment.findViews(inflater, viewGroup);
+
+            // FRONT
+            holder.frontFragment.name.setText(item.getName());
             if (item.getWeb() != null && !item.getWeb().isEmpty())
             {
-                holder.textView_web.setText(item.getWeb());
+                holder.frontFragment.web.setText(item.getWeb());
             }
             else if (item.getLocation() != null && !item.getLocation().isEmpty())
             {
-                holder.textView_web.setText(item.getLocation());
+                holder.frontFragment.web.setText(item.getLocation());
             }
             else
             {
-                holder.textView_web.setText(item.getPhone());
+                holder.frontFragment.web.setText(item.getPhone());
+            }
+            holder.frontFragment.tagAdapter.setTags(item.getTags());
+
+            // BACK
+
+            if (item.getNotes() != null && !item.getNotes().isEmpty())
+            {
+                holder.backFragment.linearLayoutNotes.setVisibility(View.VISIBLE);
+                holder.backFragment.linearLayoutNotesNoInfo.setVisibility(View.GONE);
+                holder.backFragment.notes.setText(item.getNotes());
+            }
+            else
+            {
+                holder.backFragment.linearLayoutNotes.setVisibility(View.GONE);
+                holder.backFragment.linearLayoutNotesNoInfo.setVisibility(View.VISIBLE);
             }
 
-            RecyclerView recyclerView = holder.recyclerView;
+            if (item.getUbication() != null && !item.getUbication().isEmpty())
+            {
+                holder.backFragment.linearLayoutUbication.setVisibility(View.VISIBLE);
+                holder.backFragment.linearLayoutUbicationNoInfo.setVisibility(View.GONE);
+                holder.backFragment.ubication.setText(item.getUbication());
+            }
+            else
+            {
+                holder.backFragment.linearLayoutUbication.setVisibility(View.GONE);
+                holder.backFragment.linearLayoutUbicationNoInfo.setVisibility(View.VISIBLE);
+            }
 
-            recyclerView.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false));
-            TagAdapter tagAdapter = new TagAdapter(context);
-            recyclerView.setAdapter(tagAdapter);
+            // TODO: Set onClick listener to BackCard buttons
 
-            tagAdapter.setTags(item.getTags());
-            */
         }
 
     }
@@ -122,16 +141,16 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.Holder>
         private ItemBackFragment backFragment;
         private boolean isSwapped;
 
-        private Holder (View view, final Context context, final FragmentManager fragmentManager)
+        private Holder (View view, @NonNull final FragmentManager fragmentManager)
         {
             super(view);
             isSwapped = false;
 
             frontFragment = new ItemFrontFragment();
             backFragment = new ItemBackFragment();
-            
+
             fragmentManager.beginTransaction()
-                    .add(R.id.container, frontFragment)
+                    .add(R.id.container, frontFragment, String.valueOf(getAdapterPosition()))
                     .commit();
 
             view.setOnClickListener(new View.OnClickListener()
@@ -155,7 +174,7 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.Holder>
                                     R.animator.flip_down_out,
                                     R.animator.flip_up_in,
                                     R.animator.flip_up_out)
-                            .replace(R.id.container, backFragment)
+                            .replace(R.id.container, backFragment, String.valueOf(getAdapterPosition()))
                             // Add this transaction to the back stack, allowing users to press
                             // Back to get to the front of the card.
                             .addToBackStack(null)
@@ -165,7 +184,10 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.Holder>
         }
 
         public static class ItemFrontFragment extends Fragment {
-            
+
+            private View view;
+            private Context context;
+
             private TextView name;
             private TextView web;
             private LinearLayout linearLayoutCall;
@@ -173,39 +195,59 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.Holder>
             private TagAdapter tagAdapter;
             
             @Override
-            public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
-                                     Bundle savedInstanceState) {
-                
-                View view = inflater.inflate(R.layout.holder_item_content_front, container, false);
-                Context context = getContext();
+            public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+            {
                 if (context != null)
                 {
                     Resources res = context.getResources();
                     float scale = res.getDisplayMetrics().density * res.getInteger(R.integer.factor_scale);
                     view.setCameraDistance(scale);
-                    
+                }
+                return view;
+            }
+
+            private void findViews(@NonNull LayoutInflater inflater, ViewGroup container)
+            {
+                view = inflater.inflate(R.layout.holder_item_content_front, container, false);
+                if (context != null)
+                {
                     recyclerView = view.findViewById(R.id.recyclerView_tags);
                     linearLayoutCall = view.findViewById(R.id.layout_item_call);
                     name = view.findViewById(R.id.textView_item_name);
                     web = view.findViewById(R.id.textView_item_web);
-                                        
+
                     tagAdapter = new TagAdapter(context);
-                    
+
                     recyclerView.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false));
                     recyclerView.setAdapter(tagAdapter);
                 }
-                return view;
             }
         }
 
 
         public static class ItemBackFragment extends Fragment {
+
+            private Context context;
+            private View view;
+
+            private LinearLayout linearLayoutNotes;
+            private LinearLayout linearLayoutNotesNoInfo;
+
+            private LinearLayout linearLayoutUbication;
+            private LinearLayout linearLayoutUbicationNoInfo;
+
+            private TextView notes;
+            private TextView ubication;
+
+            private ImageView iv_ubication;
+
+            private ImageView edit;
+            private ImageView hashtags;
+            private ImageView delete;
+
             @Override
             public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                                      Bundle savedInstanceState) {
-                View view = inflater.inflate(R.layout.holder_item_content_back, container, false);
-
-                Context context = getContext();
                 if (context != null)
                 {
                     Resources res = context.getResources();
@@ -213,6 +255,25 @@ public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.Holder>
                     view.setCameraDistance(scale);
                 }
                 return view;
+            }
+
+            private void findViews(@NonNull LayoutInflater inflater, ViewGroup container)
+            {
+                view = inflater.inflate(R.layout.holder_item_content_back, container, false);
+                linearLayoutNotes = view.findViewById(R.id.linearLayoutNotes);
+                linearLayoutNotesNoInfo = view.findViewById(R.id.linearLayoutNotesNoInfo);
+
+                linearLayoutUbication = view.findViewById(R.id.linearLayoutUbication);
+                linearLayoutUbicationNoInfo = view.findViewById(R.id.linearLayoutUbicationNoInfo);
+
+                notes = view.findViewById(R.id.textView_notes);
+                ubication = view.findViewById(R.id.textView_ubication);
+
+                iv_ubication = view.findViewById(R.id.imageView_button_ubication);
+
+                edit = view.findViewById(R.id.imageView_button_edit);
+                hashtags = view.findViewById(R.id.imageView_button_hashtag);
+                delete = view.findViewById(R.id.imageView_button_delete);
             }
         }
     }
